28cff239edb7d2549acb9e1ff6da149c1d3a3d8a
栗田大輔 <daikons.msn36@gmail.com>
2021-08-28 13:28:01 +0900

    タイムゾーンを日本に設定しました

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module DevExam
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.generators do |g|
       g.assets false
       g.helper false
