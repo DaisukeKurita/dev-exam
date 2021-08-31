947cce50ccd88e9a9b88653b7fd311b0a4efb45e
栗田大輔 <daikons.msn36@gmail.com>
2021-09-01 00:59:25 +0900

    app/views/properties/new.html.erbを国際化

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module DevExam
   class Application < Rails::Application
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.generators do |g|
